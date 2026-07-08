d3a94dcc076a2d6ff2a71cf9a8cbfbf1c764b95d
jcasir <casiraghi.jacopo@gmail.com>
2026-07-08 15:47:52 +0200

    fix: correct extension for 1D output file

--- include/outputWriter.hpp
+++ include/outputWriter.hpp
@@ -348,7 +348,7 @@ class OutputWriter1D {
 public:
     OutputWriter1D(const ConfigParser& cfg)
     {
-    	filename_ = cfg.getString("OUTPUT_DIR") + cfg.getString("OUTPUT_FILE") + ".cfg";
+    	filename_ = cfg.getString("OUTPUT_DIR") + cfg.getString("OUTPUT_FILE") + ".csv";
     	nx = cfg.getInt("GRID_POINTS");
 	    std::ofstream file(filename_);
 	    if (!file){
